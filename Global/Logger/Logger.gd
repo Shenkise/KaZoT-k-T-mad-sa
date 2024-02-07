0d048eb6b9695cc255573055ca7db81b81f3a2f2
Shenkise <magyaritamas01@gmail.com>
2024-02-07 17:56:53 +0200

    Logger demo and save\load 0.1

---
+++ Global/Logger/Logger.gd
@@ -0,0 +1,16 @@
+extends Node
+
+@export var logging: bool = false
+
+func _is_logging(current: Node) -> bool:
+	if current.name == "main":
+		return true
+
+	if !current.logging:
+		return false
+
+	return _is_logging(current.get_parent())
+
+func debug_log(message: String) -> void:
+	if _is_logging(self):
+		print_debug(message)
